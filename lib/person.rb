bbd82bdfaff10da4861bcaab697f0bd041c21d24
Kate Bennert <kate.bennert@gmail.com>
2023-01-21 13:50:31 -0500

    commit tree db4114093125c885a63c38281a2cda8036e7502c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+kate = Person.new({:name => "Kate", birthday: "January 19"})
+puts kate.birthday
